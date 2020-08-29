1dd662c6b57153f254eb6342e4c5d97a09a3fdea
thiagopradodemellobranco <thiagopradodemellobranco@localhost>
2020-08-29 17:47:49 -0300

    ..

--- src/main/java/less2020/controle/RoutersControle.java
+++ src/main/java/less2020/controle/RoutersControle.java
@@ -2,6 +2,7 @@ package less2020.controle;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,5 +13,11 @@ public class RoutersControle {
 		return new ModelAndView("FormCliente");
 	}
 	
+	
+	@PostMapping("SalvarCliente")
+	public ModelAndView salvarcliente() {
+		System.out.println("passei aqui");
+		return new ModelAndView("Servlet");
+	}
 
 }
